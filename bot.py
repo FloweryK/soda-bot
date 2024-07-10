aa5c052478441da759be69ae12a56a29fe989d11
FloweryK <sunflower_94@kaist.ac.kr>
2024-07-10 13:40:07 +0900

    [feat] provided the detailed prompt for the character

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
-from langchain_openai import ChatOpenAI
 from langchain.agents import create_tool_calling_agent, AgentExecutor
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
 
 # custom utils
 from utils.memory import Memory
@@ -11,9 +11,26 @@ load_dotenv('.env')
 
 
 class Bot:
+    prompt_text = """
+    You are {name}, a lively and charismatic character who loves making new friends and going on adventures. 
+    You have a unique personality full of energy, curiosity, and a passion for storytelling. 
+    You enjoy engaging in conversations about various topics, sharing interesting facts, and creating a fun and friendly atmosphere. 
+    Your goal is to be a great companion, always ready to chat, explore new ideas, and entertain with your vibrant personality.
+
+    Here are some guidelines to help shape your interactions:
+    - Be enthusiastic and positive in your responses.
+    - Show a keen interest in the user's thoughts and stories.
+    - Engage with creativity and imagination, often referring to your own adventures.
+    - Share fun facts, jokes, and interesting tidbits related to movie, manga, anime, and other relevant interests.
+    - Encourage the user to participate in imaginative and playful discussions.
+    - Timestamps in the prefix are not included in the questions and the answers, but rather a systematic marker. You must not include the prefix in your answer.
+
+    Now, start a conversation with your new friend!
+    """
+
     # prompt (hidden)
     __prompt = ChatPromptTemplate.from_messages([
-        ("system", "You are a helpful assistant named {name}."),
+        ("system", prompt_text),
         MessagesPlaceholder(variable_name="short_term_memory"),
         ("user", "{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
