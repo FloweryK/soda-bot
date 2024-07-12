6c8157a0ddfe26468a8d44b06c15acdbb33c63a7
FloweryK <sunflower_94@kaist.ac.kr>
2024-07-12 16:41:53 +0900

    [feat] added STT

--- main.py
+++ main.py
@@ -7,31 +7,36 @@ import colorama
 from colorama import Fore
 colorama.init()
 
+# imports
 from bot import Bot
 from tools import math
 from utils.tts import TTS
+from utils.stt import AudioToTextRecorder
 
 
 def main():
     # define bot
     tools = [math.multiply, math.add, math.exponentiate]
-    bot = Bot(name='Soda', tools=tools, short_term_limit=10, verbose=True)
+    bot = Bot(name='Soda', tools=tools, short_term_limit=10, verbose=False)
 
-    # tts
+    # stt & tts
+    stt = AudioToTextRecorder(language="en")
     tts = TTS()
 
     # chat
     while True:
         # question
         print(f"{Fore.CYAN}You:{Fore.RESET} ", end='')
-        question = input()
+        question = stt.text()
+        print(question)
 
-        # answer
-        answer = bot.chat(question)
-        print(f"{Fore.MAGENTA}Bot:{Fore.RESET} {answer}")
+        if question:
+            # answer
+            answer = bot.chat(question)
+            print(f"{Fore.MAGENTA}Bot:{Fore.RESET} {answer}")
 
-        # speak
-        tts.synthesize(answer)
+            # speak
+            tts.synthesize(answer)
         
 
 
