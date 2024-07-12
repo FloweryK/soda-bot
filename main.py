451691dba4e9241bc5c78162b623a0cbb094ed85
FloweryK <flowerk94@gmail.com>
2024-07-13 00:40:06 +0900

    [feat] minor refactoring

--- main.py
+++ main.py
@@ -13,36 +13,54 @@ from core.brain import Brain
 from core.tts.azure_tts import AzureTTS
 from core.stt.realtime_stt import RealtimeSTT
 
+# CONFIGS
+BOT_NAME = 'Soda'
+MEMORY_SHORT_TERM_LIMIT = 10
+USE_STT = False
+USE_TTS = False
+
 
 def main():
     # brain
-    brain = Brain(name='Soda', short_term_limit=10)
+    brain = Brain(name=BOT_NAME, short_term_limit=MEMORY_SHORT_TERM_LIMIT)
 
     # stt
-    stt = RealtimeSTT(language="en")
+    if USE_STT:
+        stt = RealtimeSTT(language="en")
+    else:
+        stt = None
 
     # tts
-    tts = AzureTTS(
-        language="en-US",
-        voice="en-US-AshleyNeural",
-        rate="+10.00%",
-        pitch="+25.00%"
-    )
+    if USE_TTS:
+        tts = AzureTTS(
+            language="en-US",
+            voice="en-US-AshleyNeural",
+            rate="+10.00%",
+            pitch="+25.00%"
+        )
+    else:
+        tts = None
+
 
     while True:
         # listen
-        print(f"{Fore.CYAN}You:{Fore.RESET} ")
-        question = stt.text()
-        print(question)
+        if USE_STT:
+            print(f"{Fore.CYAN}You:{Fore.RESET} ")
+            question = stt.text()
+            print(question, '\n')
+        else:
+            print(f"{Fore.CYAN}You:{Fore.RESET} ", end='')
+            question = input()
 
         if question:
             # answer
             answer = brain.chat(question)
-            print(f"\n{Fore.MAGENTA}Bot:{Fore.RESET} {answer}")
+            print(f"{Fore.MAGENTA}Bot:{Fore.RESET} {answer}")
 
-            # speak
-            answer_filtered = emoji.replace_emoji(answer, replace='')
-            tts.synthesize(answer_filtered)
+            if USE_TTS:
+                # speak
+                answer_filtered = emoji.replace_emoji(answer, replace='')
+                tts.synthesize(answer_filtered)
         
 
 if __name__ == "__main__":
