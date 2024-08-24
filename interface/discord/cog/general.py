8e02148308324406e51d58710cdb3316898bb303
FloweryK <sunflower_94@kaist.ac.kr>
2024-08-25 01:11:09 +0900

    [feat] added chat command to discord interface

--- interface/discord/cog/general.py
+++ interface/discord/cog/general.py
@@ -10,6 +10,12 @@ class General(commands.Cog):
     async def add(self, ctx: commands.Context[InterfaceDiscord]):
         await ctx.defer(ephemeral=True)
         await ctx.send(f"Pong! ({self.bot.latency * 1000:.2f}ms)")
+    
+    @commands.command(name="c")
+    async def chat(self, ctx: commands.Context[InterfaceDiscord], *words):
+        text = ' '.join(words)
+        for s in self.bot.brain.chat(question=text, is_stream=False):
+            await ctx.send(s['text'])
 
     
 async def setup(bot: InterfaceDiscord):
